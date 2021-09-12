833e1689dde6988c6ca805427992fce1de89f0e7
DJ <daniel.jms.walker@gmail.com>
2021-09-12 17:17:05 -0400

    Initial Commit

---
+++ teeteringFlixVer1Tests/teeteringFlixVer1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  teeteringFlixVer1Tests.swift
+//  teeteringFlixVer1Tests
+//
+//  Created by Gina Barros on 9/12/21.
+//
+
+import XCTest
+@testable import teeteringFlixVer1
+
+class teeteringFlixVer1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
